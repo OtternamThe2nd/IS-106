085fd52ee4fc8ea43307f5ddee08dc348c5cddac
Otternam <239063574+OtternamThe2nd@users.noreply.github.com>
2026-03-28 10:04:11 +0800

    Rehaul loginPage

--- Web Proj/src/Login.jsx
+++ Web Proj/src/Login.jsx
@@ -9,16 +9,8 @@ function Login() {
   return (
   <>
     <Header/>
-    <div style={{backgroundImage:`url(${bgBuilding})`}} class="flex relative bg-cover h-200 bg-center h-200">
-      <div class="absolute inset-0 bg-gradient-to-r from-slate-950 to-slate-150 h-200 flex opacity-50">
-      </div>
-      <div class ="relative z-1 md:hidden sm:hidden lg:block mt-auto text-5xl ml-10 mb-25 font-black font-[glacial]">
-        <p class="p-2 bg-red-500 rounded-t-md text-rose-50">Taguig's Healthcare</p>
-        <p class="w-fit p-2 bg-red-500 rounded-b-md text-rose-50">Center</p>
-      </div>
-      <div class="relative z-1 opacity-100 flex mr-auto ml-auto lg:mr-10 h-200">
-          <Form/>
-      </div>
+    <div class="flex relative bg-cover h-200 bg-center h-200">
+      <Form/>
     </div>
   </>
   )
